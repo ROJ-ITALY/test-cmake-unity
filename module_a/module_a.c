c8611f9cdac6d9fcd9f7d3285768f3f33349f445
Paola Martiner Giore <50236341+paolamgc@users.noreply.github.com>
2022-09-20 16:27:22 +0200

    Update module_a.c
    - Manage rounding with negative results.

--- module_a/module_a.c
+++ module_a/module_a.c
@@ -2,5 +2,6 @@
 
 int8_t AverageThreeBytes(int8_t a, int8_t b, int8_t c)
 {
-    return (int8_t)(((float)a + (float)b + (float)c + 0.5) / 3.0);
+    float sum = (float)a + (float)b + (float)c;
+    return (int8_t)((sum + (sum>0?0.5:((sum==0)?0:-0.5))) / 3.0);
 }
